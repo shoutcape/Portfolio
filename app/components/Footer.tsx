f8703ba34b55beca2cf11866ffb8c1476168369a
Shoutcape <kautiainen.ville@gmail.com>
2024-10-28 20:27:19 +0200

    Footer done, next projects.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import styles from './Footer.module.css'
+import LinkedinSVG from '@/../public/social-media-svgs/linkedin.svg'
+import GithubSVG from '@/../public/social-media-svgs/github.svg'
 
 const Footer = () => {
   return (
-    <div className={`${styles.footerContainer}`}>
-      <h1 className='text-3xl'>Footer</h1>
-    </div>
+    <footer className="footer footer-center bg-base-300 p-10">
+      <nav>
+        <div className="grid grid-flow-col gap-10">
+          <a className='w-10' href='https://www.linkedin.com/in/villekautiainen/'>
+            <LinkedinSVG />
+          </a>
+          <a className='w-10' href='https://github.com/shoutcape'>
+            <GithubSVG />
+          </a>
+        </div>
+      </nav>
+      <h3 className='text-lg'> kautiainen.ville@gmail.com </h3>
+    </footer>
   )
 }
 
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,15 +1,15 @@
-'use client';
 import styles from './Projects.module.css'
 
 const Projects = () => {
+
   return (
     <div className='mt-10 flex flex-col items-center'>
       <h1 className='text-3xl mb-10'>Projects</h1>
       <div className={`${styles.projectContainer}`}>
         <div className={`${styles.textContainer}`}>
-          <h1 className='text-3xl'>Example Project</h1>
+          <h1 className='text-3xl pb-5'>Example Project</h1>
           <div>
-            <p>A web app for visualizing personalized Spotify data. View your
+            <p className='text-xl'>A web app for visualizing personalized Spotify data. View your
               top artists, top tracks, recently played tracks, and detailed audio
               information about each track. Create and save new playlists of
               recommended tracks based on your existing playlists and more.</p>
@@ -21,9 +21,9 @@ const Projects = () => {
 
       <div className={`${styles.projectContainerReverse}`}>
         <div className={`${styles.textContainer}`}>
-          <h1 className='text-3xl'>Example Project</h1>
+          <h1 className='text-3xl pb-5'>Example Project</h1>
           <div>
-            <p>A web app for visualizing personalized Spotify data. View your
+            <p className='text-xl'>A web app for visualizing personalized Spotify data. View your
               top artists, top tracks, recently played tracks, and detailed audio
               information about each track. Create and save new playlists of
               recommended tracks based on your existing playlists and more.</p>
