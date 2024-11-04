b768d73b92e29fc84a16dfba45e7c7e3ac68af35
Shoutcape <kautiainen.ville@gmail.com>
2024-11-04 12:53:49 +0200

    changed bio text heading

--- app/components/Bio.tsx
+++ app/components/Bio.tsx
@@ -4,8 +4,7 @@ const Bio = () => {
   return (
     <div className={styles.container}>
       <div >
-        <h1 className='text-4xl'>You’re a Wizard Harry.</h1>
-        <h2 className='text-xl'>I’m a Software developer.</h2>
+        <h1 className='text-4xl'>I’m a Software developer.</h1>
         <p className='mt-10 text-3xl'>Passionate about building efficient, real-world solutions. With a hands-on approach, I delve into everything from Docker containers to real-time web applications. I enjoy solving complex tech challenges, always with an eye on optimization. Welcome to my portfolio, where I showcase some of my favorite projects.</p>
       </div>
     </div>
